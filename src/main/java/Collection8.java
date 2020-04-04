81d4b687b6f3105bad64e4f936ec3acd809d98da
thabomonamodi0 <thabomonamodi0@gmail.com>
2020-04-04 17:50:24 +0200

    step closer

--- src/main/java/Collection8.java
+++ src/main/java/Collection8.java
@@ -1,26 +1,31 @@
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 
 public class Collection8
 {
     public static void main(String[] args)
     {
-        Queue<Integer>queue = new LinkedList<Integer>();
+        Deque<Integer>queue = new LinkedList<Integer>();
         Set<Integer>set = new HashSet<Integer>();
-        int intNumber = 5;
+        int intNumber = 9;
         queue.add(6);
         queue.add(2);
         queue.add(6);
         queue.add(8);
         queue.add(3);
-        queue.add(6);
-        queue.add(6);
+        queue.add(7);
+        queue.add(1);
+        queue.add(4);
+        queue.add(4);
         //int[] subArray =
         set.addAll(queue);
+        List<Integer> list = Arrays.asList(6,2,6,8,3,7,1,4,4);
 
-        System.out.println("queue = "+queue);
+        int n = 3;
+        for (int i = 0; i <= list.size()-n; i++)
+        {
+            System.out.println(list.subList(i,i + n));
+        }
+        System.out.println("queue = "+list);
         System.out.println("Output:\n"+set.size());
     }
 }
